9caabeb407b59e22afa1ea9a71e0528a2a298a97
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-06-21 21:33:50 +0300

    rework logic, change calibration data

--- aria102_sketch_may23a.ino
+++ aria102_sketch_may23a.ino
@@ -9,6 +9,7 @@
 #define SENSOR_B_MAX 867
 
 #define DC_MIN_LEVEL 0
+#define DC_MID_LEVEL 64
 #define DC_MAX_LEVEL 255
 
 #define FIRST_SPEED 33 + 1 / 3
@@ -19,9 +20,6 @@
 #define MODE_RUN 1
 #define MODE_BREAK 2
 
-unsigned int rawSensorValue[2] = { 0, 0 };
-int calibratedSensorValue[2] = { 0, 0 };
-
 struct SensorCheckpoint {
   byte sensorId;
   int triggerLevel;
@@ -29,74 +27,47 @@ struct SensorCheckpoint {
   unsigned int offsetPercent;
 };
 
+struct StabilizerState {
+  byte maxValue;
+  byte minValue;
+  byte currentValue;
+};
+
+
 const byte sensorCheckpointsCount = 80;
 SensorCheckpoint checkpoints[sensorCheckpointsCount] = {
-  { 1, 0,  1,    0 }, { 1,  100,  1,  105 }, { 1,  200,  1,  138 }, { 0, -200,  1,  180 }, { 0, -100,  1,  308 },
-  { 0, 0,  1,  413 }, { 0,  100,  1,  518 }, { 0,  200,  1,  638 }, { 1,  200, -1,  833 }, { 1,  100, -1,  953 },
-  { 1, 0, -1, 1058 }, { 1, -100, -1, 1163 }, { 1, -200, -1, 1283 }, { 0,  200, -1, 1455 }, { 0,  100, -1, 1575 },
-  { 0, 0, -1, 1688 }, { 0, -100, -1, 1800 }, { 0, -200, -1, 1928 }, { 1,  -200, 1, 2108 }, { 1, -100,  1, 2228 },
+  { 1, 0,  1, 110 }, { 1,  100,  1, 114 }, { 1,  200,  1, 118 }, { 0, -200,  1,  98 }, { 0, -100,  1, 129 },
+  { 0, 0,  1, 110 }, { 0,  100,  1, 118 }, { 0,  200,  1, 122 }, { 1,  200, -1, 161 }, { 1,  100, -1, 118 },
+  { 1, 0, -1, 114 }, { 1, -100, -1, 114 }, { 1, -200, -1, 122 }, { 0,  200, -1, 149 }, { 0,  100, -1, 126 },
+  { 0, 0, -1, 118 }, { 0, -100, -1, 118 }, { 0, -200, -1, 122 }, { 1,  -200, 1, 181 }, { 1, -100,  1, 118 },
 
-  { 1, 0,  1, 2341 }, { 1,  100,  1, 2446 }, { 1,  200,  1, 2573 }, { 0, -200,  1, 2678 }, { 0, -100,   1, 2806 },
-  { 0, 0,  1, 2918 }, { 0,  100,  1, 3023 }, { 0,  200,  1, 3151 }, { 1,  200, -1, 3308 }, { 1,  100,  -1, 3436 },
-  { 1, 0, -1, 3548 }, { 1, -100, -1, 3653 }, { 1, -200, -1, 3773 }, { 0,  200, -1, 3931 }, { 0,  100,  -1, 4059 },
-  { 0, 0, -1, 4171 }, { 0, -100, -1, 4291 }, { 0, -200, -1, 4419 }, { 1, -200,  1, 4599 }, { 1,  -100,  1, 4719 },
+  { 1, 0,  1, 110 }, { 1,  100,  1, 110 }, { 1,  200,  1, 114 }, { 0, -200,  1, 114 }, { 0, -100,   1, 126 },
+  { 0, 0,  1, 110 }, { 0,  100,  1, 114 }, { 0,  200,  1, 118 }, { 1,  200, -1, 196 }, { 1,  100,  -1, 114 },
+  { 1, 0, -1, 110 }, { 1, -100, -1, 110 }, { 1, -200, -1, 110 }, { 0,  200, -1, 173 }, { 0,  100,  -1, 118 },
+  { 0, 0, -1, 114 }, { 0, -100, -1, 110 }, { 0, -200, -1, 122 }, { 1, -200,  1, 232 }, { 1,  -100,  1, 106 },
 
-  { 1, 0,  1, 4824 }, { 1,  100,  1, 4929 }, { 1,  200,  1, 5056 }, { 0, -200,  1, 5161 }, { 0, -100,  1, 5289 },
-  { 0, 0,  1, 5401 }, { 0,  100,  1, 5514 }, { 0,  200,  1, 5626 }, { 1,  200, -1, 5821 }, { 1,  100, -1, 5941 },
-  { 1, 0, -1, 6054 }, { 1, -100, -1, 6159 }, { 1, -200, -1, 6272 }, { 0,  200, -1, 6444 }, { 0,  100, -1, 6564 },
-  { 0, 0, -1, 6684 }, { 0, -100, -1, 6789 }, { 0, -200, -1, 6909 }, { 1, -200,  1, 7134 }, { 1, -100,  1, 7247 },
+  { 1, 0,  1, 110 }, { 1,  100,  1, 102 }, { 1,  200,  1, 114 }, { 0, -200,  1, 122 }, { 0, -100,  1, 122 },
+  { 0, 0,  1, 110 }, { 0,  100,  1, 110 }, { 0,  200,  1, 118 }, { 1,  200, -1, 181 }, { 1,  100, -1, 114 },
+  { 1, 0, -1, 110 }, { 1, -100, -1, 110 }, { 1, -200, -1, 110 }, { 0,  200, -1, 169 }, { 0,  100, -1, 122 },
+  { 0, 0, -1, 118 }, { 0, -100, -1, 106 }, { 0, -200, -1, 126 }, { 1, -200,  1, 189 }, { 1, -100,  1, 110 },
 
-  { 1, 0,  1, 7359 }, { 1,  100,  1, 7464 }, { 1,  200,  1, 7577 }, { 0, -200,  1, 7704 }, { 0, -100,  1, 7817 },
-  { 0, 0,  1, 7937 }, { 0,  100,  1, 8042 }, { 0,  200,  1, 8155 }, { 1,  200, -1, 8335 }, { 1,  100, -1, 8447 },
-  { 1, 0, -1, 8567 }, { 1, -100, -1, 8672 }, { 1, -200, -1, 8785 }, { 0,  200, -1, 8957 }, { 0,  100, -1, 9077 },
-  { 0, 0, -1, 9190 }, { 0, -100, -1, 9302 }, { 0, -200, -1, 9422 }, { 1, -200,  1, 9717 }, { 1, -100,  1, 9822 },
+  { 1, 0,  1, 110 }, { 1,  100,  1, 102 }, { 1,  200,  1, 114 }, { 0, -200,  1, 126 }, { 0, -100,  1, 118 },
+  { 0, 0,  1, 110 }, { 0,  100,  1, 110 }, { 0,  200,  1, 114 }, { 1,  200, -1, 204 }, { 1,  100, -1, 114 },
+  { 1, 0, -1, 106 }, { 1, -100, -1, 110 }, { 1, -200, -1, 114 }, { 0,  200, -1, 173 }, { 0,  100, -1, 118 },
+  { 0, 0, -1, 114 }, { 0, -100, -1, 106 }, { 0, -200, -1, 126 }, { 1, -200,  1, 189 }, { 1, -100,  1, 118 },
 };
-float checkpointTime[sensorCheckpointsCount];
+
+unsigned int rawSensorValue[2] = { 0, 0 };
+int calibratedSensorValue[2] = { 0, 0 };
+
+StabilizerState stabilizerState = { DC_MIN_LEVEL, DC_MAX_LEVEL, 43 };
+int acceleration = 0;
+
 byte currentCheckpoint = 0;
-unsigned long lastCheckpointTime = 0;
 
 // current selected speed
 int currentMode = MODE_RUN;
 int currentSpeed = SECOND_SPEED;
-
-// current sensors raw values (0..1023)
-int sensorPhaseA = 0;
-int sensorPhaseB = 0;
-
-// current sensors calibrated values (-256..256)
-int calSensorPhaseA = 0;
-int calSensorPhaseB = 0;
-
-
-// sensors checkpoints
-const int sensorsCheckpointCount = 32;
-int sensorsCheckpointMap[sensorsCheckpointCount] = {
-  0, 100, 200, 100, 0, -100, -200, -100,
-  0, 100, 200, 100, 0, -100, -200, -100,
-  0, 100, 200, 100, 0, -100, -200, -100,
-  0, 100, 200, 100, 0, -100, -200, -100,
-};
-int sensorsCheckpointDirMap[sensorsCheckpointCount] = {
-  1, 1, 1, -1, -1, -1, -1, 1,
-  1, 1, 1, -1, -1, -1, -1, 1,
-  1, 1, 1, -1, -1, -1, -1, 1,
-  1, 1, 1, -1, -1, -1, -1, 1,
-};
-int sensorACheckpointOffsets[sensorsCheckpointCount] = {
-  0, 2, 3, 100, 114, 130, 145, 267,
-  282, 296, 312, 434, 449, 464, 480, 597,
-  612, 626, 642, 766, 782, 796, 813, 929,
-  945, 960, 976, 1097, 1113, 1128, 1144, 1261,
-};
-
-unsigned long sensorACheckpointTime[sensorsCheckpointCount];
-unsigned long sensorBCheckpointTime[sensorsCheckpointCount];
-byte currentSensorACheckpoint = 0;
-byte currentSensorBCheckpoint = 0;
-
-// power
-float dcValue = DC_MAX_LEVEL;
-float dcCorrection = 0.0;
 
 void setup() {
   pinMode(3, OUTPUT);
@@ -104,114 +75,31 @@ void setup() {
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
 
-  initializeSensorsCheckpoints();
-
   if (DEBUG_MODE == false) {
-    TCCR2B = TCCR2B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
-    TCCR1B = TCCR1B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
+    //    TCCR2B = TCCR2B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
+    //    TCCR1B = TCCR1B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
   }
 
   Serial.begin(115200);
 }
 
 void loop() {
-  if (currentMode == MODE_RUN) {
-    readSensors();
-    waitNextSensorCheckpoint();
-    sendLevelsToSoils();
-    runStopDetectionTask();
-    listenToSensorCheckpointCrossing();
-  }
-  if (currentMode == MODE_STOP) {
-    cleanLevels();
-  }
+  taskReadSensors();
+  taskCheckpointCross();
+  taskSyncMotorCoils();
 }
 
-void initializeSensorsCheckpoints() {
-  for (byte i = 0; i < sensorsCheckpointCount; i += 1) {
-    sensorACheckpointTime[i] = 0;
-    sensorACheckpointTime[i] = 0;
-  }
+void taskReadSensors() {
+  rawSensorValue[0] = analogRead(SENSOR_A);
+  rawSensorValue[1] = analogRead(SENSOR_B);
+
+  calibratedSensorValue[0] = constrain(map(rawSensorValue[0], SENSOR_A_MIN, SENSOR_A_MAX, -255, 255), -255, 255);
+  calibratedSensorValue[1] = constrain(map(rawSensorValue[1], SENSOR_B_MIN, SENSOR_B_MAX, -255, 255), -255, 255);
 }
 
-void waitNextSensorCheckpoint() {
-  int expectedRoundTime = (1 / (currentSpeed / 60.0) * 1000.0);
-
-  int sensorACheckpointValue = sensorsCheckpointMap[currentSensorACheckpoint];
-  int sensorACheckpointDir = sensorsCheckpointDirMap[currentSensorACheckpoint];
-
-  if (sensorACheckpointDir * calSensorPhaseA >= sensorACheckpointDir * sensorACheckpointValue) {
-    unsigned long currentTime = millis();
-    long actualRoundTime = currentTime - sensorACheckpointTime[currentSensorACheckpoint];
-
-    int computedDcValue = map(actualRoundTime, expectedRoundTime - 100, expectedRoundTime + 100, DC_MIN_LEVEL, DC_MAX_LEVEL);
-
-    dcValue = constrain(computedDcValue, DC_MIN_LEVEL, DC_MAX_LEVEL);
-
-    applyMicroCorrection(actualRoundTime, expectedRoundTime);
-
-    sensorACheckpointTime[currentSensorACheckpoint] = currentTime;
-    currentSensorACheckpoint = (currentSensorACheckpoint + 1) % sensorsCheckpointCount;
-  }
-
-  int sensorBCheckpointValue = sensorsCheckpointMap[currentSensorBCheckpoint];
-  int sensorBCheckpointDir = sensorsCheckpointDirMap[currentSensorBCheckpoint];
-
-  if (sensorACheckpointDir * calSensorPhaseA >= sensorACheckpointDir * sensorACheckpointValue) {
-    unsigned long currentTime = millis();
-    long actualRoundTime = currentTime - sensorBCheckpointTime[currentSensorBCheckpoint];
-
-    int computedDcValue = map(actualRoundTime, expectedRoundTime - 100, expectedRoundTime + 100, DC_MIN_LEVEL, DC_MAX_LEVEL);
-
-    dcValue = constrain(computedDcValue, DC_MIN_LEVEL, DC_MAX_LEVEL);
-
-    applyMicroCorrection(actualRoundTime, expectedRoundTime);
-
-    sensorBCheckpointTime[currentSensorBCheckpoint] = currentTime;
-    currentSensorBCheckpoint = (currentSensorBCheckpoint + 1) % sensorsCheckpointCount;
-  }
-}
-
-void readSensors() {
-  sensorPhaseA = analogRead(SENSOR_A);
-  sensorPhaseB = analogRead(SENSOR_B);
-
-  calSensorPhaseA = map(sensorPhaseA, SENSOR_A_MIN, SENSOR_A_MAX, -256, 255);
-  calSensorPhaseB = map(sensorPhaseB, SENSOR_B_MIN, SENSOR_B_MAX, -256, 255);
-
-  rawSensorValue[0] = sensorPhaseA;
-  rawSensorValue[1] = sensorPhaseB;
-
-  calibratedSensorValue[0] = calSensorPhaseA;
-  calibratedSensorValue[1] = calSensorPhaseB;
-}
-
-
-void applyMicroCorrection(long actualRoundTime, long expectedRoundTime) {
-  int timeDifference = abs(actualRoundTime - expectedRoundTime);
-  float correctionRate = constrain(map(timeDifference, 0, 50, 0, 20) / 100.0, .0, .2);
-
-  if (actualRoundTime < expectedRoundTime) {
-    // too fast
-    dcCorrection = max(-100.0, dcCorrection - correctionRate);
-  }
-
-  if (actualRoundTime > expectedRoundTime) {
-    // too slow
-    dcCorrection = min(100.0, dcCorrection + correctionRate);
-  }
-}
-
-void cleanLevels() {
-  analogWrite(3, 0);
-  analogWrite(9, 0);
-  analogWrite(10, 0);
-  analogWrite(11, 0);
-}
-
-void sendLevelsToSoils() {
-  int amplifiedValueA = min(255.0, dcCorrection + dcValue) / 255.0 * calSensorPhaseA;
-  int amplifiedValueB = min(255.0, dcCorrection + dcValue) / 255.0 * calSensorPhaseB;
+void taskSyncMotorCoils() {
+  int amplifiedValueA = min(255, stabilizerState.currentValue) / 255.0 * calibratedSensorValue[0];
+  int amplifiedValueB = min(255, stabilizerState.currentValue) / 255.0 * calibratedSensorValue[1];
 
   analogWrite(3, constrain(max(0, amplifiedValueA), 0, 255));
   analogWrite(9, constrain(-min(0, amplifiedValueB), 0, 255));
@@ -219,14 +107,14 @@ void sendLevelsToSoils() {
   analogWrite(11, constrain(max(0, amplifiedValueB), 0, 255));
 }
 
-void runStopDetectionTask() {
-  if (millis() - sensorACheckpointTime[0] > 2000) {
-    dcValue = DC_MAX_LEVEL;
-  }
-}
+unsigned long lastCheckpointTime = 0;
+unsigned long prevCycleTime = 0;
+unsigned long prevRoundTime = 0;
 
-void listenToSensorCheckpointCrossing() {
-  int expectedRoundTime = (1 / (currentSpeed / 60.0) * 1000.0);
+unsigned int calibrationLastTime = 0;
+
+void taskCheckpointCross() {
+  int expectedRoundTime = (1 / (currentSpeed / 60.0) * 250.0);
 
   SensorCheckpoint sc = checkpoints[currentCheckpoint];
   int level = sc.triggerLevel;
@@ -236,19 +124,39 @@ void listenToSensorCheckpointCrossing() {
   if (sign * sensorValue >= sign * level) {
     unsigned long now = millis();
     unsigned int expectedPercent = currentCheckpoint == 0
-      ? (10000 - checkpoints[sensorCheckpointsCount - 1].offsetPercent)
-      : (sc.offsetPercent - checkpoints[currentCheckpoint - 1].offsetPercent);
+                                   ? (10000 - checkpoints[sensorCheckpointsCount - 1].offsetPercent)
+                                   : (sc.offsetPercent - checkpoints[currentCheckpoint - 1].offsetPercent);
 
     int expectedCheckpointTime = expectedPercent / 10000.0 * expectedRoundTime;
     int actualCheckpointTime = now - lastCheckpointTime;
 
-    Serial.print(currentCheckpoint);
-    Serial.print('\t');
-    Serial.print(expectedPercent);
-    Serial.print('\t');
-    Serial.println(actualCheckpointTime - expectedCheckpointTime);
+    if (currentCheckpoint == 0) {
+      unsigned long actualRoundTime = now - prevCycleTime;
+      Serial.print(actualRoundTime);
+      Serial.print(' ');
+      prevCycleTime = now;
+    }
+
+    if (currentCheckpoint % 20 == 0) {
+//      unsigned long actualRoundTime = now - prevCycleTime;
+
+//      if (prevCycleTime > 0) {
+//        acceleration = constrain((int) prevRoundTime - (int) actualRoundTime, -50, 50);
+//
+//        prevRoundTime = actualRoundTime;
+//      }
+
+//      prevCycleTime = now;
+    }
+
+    Serial.print(now - lastCheckpointTime);
+    Serial.print(' ');
 
     lastCheckpointTime = now;
     currentCheckpoint = (currentCheckpoint + 1) % sensorCheckpointsCount;
+
+    if (currentCheckpoint == 0) {
+        Serial.print('\n');
+    }
   }
 }
