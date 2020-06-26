6fba201de94f503caa03b427591184e1f17017b7
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2020-06-26 19:23:31 +0300

    simplify algorithm

--- aria102_sketch_may23a.ino
+++ aria102_sketch_may23a.ino
@@ -1,23 +1,22 @@
-#define DEBUG_MODE false
+#define DEBUG_MODE            false
 
-#define SENSOR_A A1
-#define SENSOR_B A0
+#define SENSOR_A              A1
+#define SENSOR_B              A0
 
-#define SENSOR_A_MIN 190
-#define SENSOR_A_MAX 869
-#define SENSOR_B_MIN 186
-#define SENSOR_B_MAX 867
+#define OUTPUT_ANCHOR_A       3
+#define OUTPUT_ANCHOR_B       9
+#define OUTPUT_ANCHOR_C       10
+#define OUTPUT_ANCHOR_D       11
 
-#define DC_MIN_LEVEL 0
-#define DC_MID_LEVEL 64
-#define DC_MAX_LEVEL 255
+#define SENSOR_A_MIN          190
+#define SENSOR_A_MAX          869
+#define SENSOR_B_MIN          186
+#define SENSOR_B_MAX          867
+#define SENSOR_CAL_MIN        -255
+#define SENSOR_CAL_MAX        255
 
-#define STATE_STANDBY         0
-#define STATE_ACCELERATE      1
-#define STATE_SYNC            2
-#define STATE_GENERATOR       3
-#define STATE_BREAK           4
-
+#define PWM_LEVEL_MIN         0
+#define PWM_LEVEL_MAX         255
 
 struct SensorCheckpoint {
   byte sensorId;
@@ -26,14 +25,16 @@ struct SensorCheckpoint {
   byte offsetPercent;
 };
 
-struct StabilizerState {
-  byte maxValue;
-  byte minValue;
-  float currentValue;
+struct Speed {
+  float rpm;
+  float fineTune;
 };
 
-
 const byte sensorCheckpointsCount = 80;
+const byte speedsCount = 3;
+const byte sensorsCount = 2;
+const byte roundsCount = 4;
+
 SensorCheckpoint checkpoints[sensorCheckpointsCount] = {
   { 1, 0,  1, 110 }, { 1,  100,  1, 114 }, { 1,  200,  1, 118 }, { 0, -200,  1,  98 }, { 0, -100,  1, 129 },
   { 0, 0,  1, 110 }, { 0,  100,  1, 118 }, { 0,  200,  1, 122 }, { 1,  200, -1, 161 }, { 1,  100, -1, 118 },
@@ -56,75 +57,76 @@ SensorCheckpoint checkpoints[sensorCheckpointsCount] = {
   { 0, 0, -1, 114 }, { 0, -100, -1, 106 }, { 0, -200, -1, 126 }, { 1, -200,  1, 189 }, { 1, -100,  1, 118 },
 };
 
-unsigned int rawSensorValue[2] = { 0, 0 };
-int calibratedSensorValue[2] = { 0, 0 };
+unsigned int rawSensorValue[sensorsCount] = { 0, 0 };
+int calibratedSensorValue[sensorsCount] = { 0, 0 };
+unsigned long fullRoundCheckpoints[roundsCount] = { 0, 0, 0, 0 };
 
-const byte speedsCount = 3;
-float rotationSpeeds[speedsCount] = {
-  33.0 + 1 / 3.0,
-  45.0,
-  78,
+Speed rotationSpeeds[speedsCount] = {
+  { 33.0 + 1 / 3.0, 40.0 },
+  { 45.0, 10.0 },
 };
 
-StabilizerState stabilizerState = { DC_MIN_LEVEL, DC_MAX_LEVEL, DC_MAX_LEVEL };
-int acceleration = 0;
-
+float pwmValue = 128.0;
 byte currentCheckpoint = 0;
+float speedAccuracyRatio = 0.0;
 
-// Current Program State
 byte speed = 0;
 
-// Sine Generator
-unsigned long sineSyncMillis = 0;
+float getExpectedRoundTime() {
+  return (1 / (rotationSpeeds[speed].rpm / 60.0) * 1000.0);
+}
+
+float getSpeedFineTune() {
+  return rotationSpeeds[speed].fineTune;
+}
 
 void setup() {
-  pinMode(3, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(OUTPUT_ANCHOR_A, OUTPUT);
+  pinMode(OUTPUT_ANCHOR_B, OUTPUT);
+  pinMode(OUTPUT_ANCHOR_C, OUTPUT);
+  pinMode(OUTPUT_ANCHOR_D, OUTPUT);
 
   if (DEBUG_MODE == false) {
-        TCCR2B = TCCR2B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
-        TCCR1B = TCCR1B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
+    TCCR2B = TCCR2B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
+    TCCR1B = TCCR1B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
   }
-
-  Serial.begin(9600);
 }
 
 void loop() {
   taskReadSensors();
-  taskCheckpointCross();
-  taskSyncMotorCoils();
-  taskSyncAverages();
-  taskAdjustDcPower();
+  taskControlRotation();
+  taskSyncAnchorsPwmValues();
 }
 
 void taskReadSensors() {
   rawSensorValue[0] = analogRead(SENSOR_A);
   rawSensorValue[1] = analogRead(SENSOR_B);
 
-  calibratedSensorValue[0] = constrain(map(rawSensorValue[0], SENSOR_A_MIN, SENSOR_A_MAX, -255, 255), -255, 255);
-  calibratedSensorValue[1] = constrain(map(rawSensorValue[1], SENSOR_B_MIN, SENSOR_B_MAX, -255, 255), -255, 255);
+  calibratedSensorValue[0] = constrain(
+                               map(
+                                 rawSensorValue[0],
+                                 SENSOR_A_MIN,
+                                 SENSOR_A_MAX,
+                                 SENSOR_CAL_MIN,
+                                 SENSOR_CAL_MAX
+                               ),
+                               SENSOR_CAL_MIN,
+                               SENSOR_CAL_MAX
+                             );
+  calibratedSensorValue[1] = constrain(
+                               map(
+                                 rawSensorValue[1],
+                                 SENSOR_B_MIN,
+                                 SENSOR_B_MAX,
+                                 SENSOR_CAL_MIN,
+                                 SENSOR_CAL_MAX
+                               ),
+                               SENSOR_CAL_MIN,
+                               SENSOR_CAL_MAX
+                             );
 }
 
-void taskSyncMotorCoils() {
-  int amplifiedValueA = min(255, stabilizerState.currentValue) / 255.0 * calibratedSensorValue[0];
-  int amplifiedValueB = min(255, stabilizerState.currentValue) / 255.0 * calibratedSensorValue[1];
-
-  analogWrite(3, constrain(max(0, amplifiedValueA), 0, 255));
-  analogWrite(9, constrain(-min(0, amplifiedValueB), 0, 255));
-  analogWrite(10, constrain(-min(0, amplifiedValueA), 0, 255));
-  analogWrite(11, constrain(max(0, amplifiedValueB), 0, 255));
-}
-
-unsigned long lastCheckpointTime = 0;
-
-float speedAccuracyRatio = 0.0;
-float speedAccuracyRatioAverage = 0.0;
-
-unsigned long last = 0;
-
-void taskCheckpointCross() {
+void taskControlRotation() {
   int expectedRoundTime = getExpectedRoundTime();
 
   SensorCheckpoint sc = checkpoints[currentCheckpoint];
@@ -138,42 +140,38 @@ void taskCheckpointCross() {
                                    ? checkpoints[sensorCheckpointsCount - 1].offsetPercent
                                    : sc.offsetPercent;
 
-    float expectedCheckpointTime = expectedPercent / 10000.0 * expectedRoundTime;
-    int actualCheckpointTime = now - lastCheckpointTime;
-
-    speedAccuracyRatio = 100 / actualCheckpointTime * expectedCheckpointTime;
-
-
-    lastCheckpointTime = now;
     currentCheckpoint = (currentCheckpoint + 1) % sensorCheckpointsCount;
 
     if (currentCheckpoint % 20 == 0) {
-      Serial.println(now - last);
-      last = now;
+      unsigned long previousRound = fullRoundCheckpoints[currentCheckpoint / 20];
+      long actualRoundTime = now - previousRound;
+      fullRoundCheckpoints[currentCheckpoint / 20] = now;
+
+      speedAccuracyRatio = 100 / (float) actualRoundTime * (float) expectedRoundTime;
+      taskSyncPwmToRotationSpeed();
     }
   }
 }
 
-unsigned long prevSyncAveragesTime = 0;
-void taskSyncAverages() {
-  unsigned long now = millis();
-  if (now - prevSyncAveragesTime > 10) {
-    speedAccuracyRatioAverage += (speedAccuracyRatio - speedAccuracyRatioAverage) * 0.5;
-    prevSyncAveragesTime = now;
+void taskSyncPwmToRotationSpeed() {
+  if (speedAccuracyRatio < 80) {
+    pwmValue = PWM_LEVEL_MAX;
+    return;
   }
+  float fineTune = getSpeedFineTune();
+  pwmValue = constrain(map(speedAccuracyRatio * 10.0 + fineTune, 800, 1050, 100, 64), 0, 255);
 }
 
-long n = 0;
-void taskAdjustDcPower() {
-  int dc = map((speedAccuracyRatioAverage) * 10.0, 175.0, 1175.0, DC_MAX_LEVEL, DC_MIN_LEVEL);
-  dc = constrain(dc, DC_MIN_LEVEL, DC_MAX_LEVEL);
-  stabilizerState.currentValue += (dc - stabilizerState.currentValue) * 0.5;
-}
+void taskSyncAnchorsPwmValues() {
+  int amplifiedValueA = min(255, pwmValue) / 255.0 * calibratedSensorValue[0];
+  int amplifiedValueB = min(255, pwmValue) / 255.0 * calibratedSensorValue[1];
 
-float getExpectedRoundTime() {
-  return (1 / (rotationSpeeds[speed] / 60.0) * 1000.0);
+  analogWrite(OUTPUT_ANCHOR_A, constrain(max(0, amplifiedValueA), 0, 255));
+  analogWrite(OUTPUT_ANCHOR_B, constrain(-min(0, amplifiedValueB), 0, 255));
+  analogWrite(OUTPUT_ANCHOR_C, constrain(-min(0, amplifiedValueA), 0, 255));
+  analogWrite(OUTPUT_ANCHOR_D, constrain(max(0, amplifiedValueB), 0, 255));
 }
 
 float amplifySensorValue(float sensorValue) {
-  return min(255, stabilizerState.currentValue) / 255.0 * sensorValue;
+  return min(255, pwmValue) / 255.0 * sensorValue;
 }
